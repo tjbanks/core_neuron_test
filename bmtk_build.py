ac2c3b9727f4891c944b385603c395225e84116f
gregglickert <gglick9@gmail.com>
2023-01-24 10:33:31 -0600

    renamed

--- bmtk_build.py
+++ bmtk_build.py
@@ -48,35 +48,5 @@ build_env_bionet(base_dir='./',
                 compile_mechanisms=False,
                 overwrite_config=True)
 
-def run(config_file):
-    warnings.simplefilter(action='ignore', category=FutureWarning)
-    synapses.load()
 
-    conf = bionet.Config.from_json(config_file, validate=True)
-    conf.build_env()
-
-    graph = bionet.BioNetwork.from_config(conf)
-
-    # This fixes the morphology error in LFP calculation
-    pop = graph._node_populations['biophysical']
-    for node in pop.get_nodes():
-        node._node._node_type_props['morphology'] = node.model_template[1]
-
-    sim = bionet.BioSimulator.from_config(conf, network=graph)
-
-    # This calls insert_mechs() on each cell to use its gid as a seed
-    # to the random number generator, so that each cell gets a different
-    # random seed for the point-conductance noise
-    cells = graph.get_local_cells()
-    for cell in cells:
-        cells[cell].hobj.insert_mechs(cells[cell].gid)
-        pass
-    sim.run()
-    bionet.nrn.quit_execution()
-
-run('config.json')
-
-F = h5py.File('output/v_report.h5')
-plt.plot(F['report']['biophysical']['data'][:])
-plt.savefig('bmtk_test.png')
 
